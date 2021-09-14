c5c282cfbcf7f7669c36c7585c5b86521255f79e
lucasSCsantos <luc.cristovam10@gmail.com>
2021-09-13 23:03:15 -0300

    defaultLayout, pages e routes

---
+++ src/Pages/Login.js
@@ -0,0 +1,14 @@
+import Layout, { Content } from 'antd/lib/layout/layout';
+import React from 'react';
+
+const Login = () => {
+	return (
+		<Layout>
+			<Content> 
+	
+			</Content>
+		</Layout>
+	);
+}
+
+export default Login;
