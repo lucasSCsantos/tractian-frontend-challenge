21bd8509574f6c08f7faf418101362e8be4776f7
lucasSCsantos <luc.cristovam10@gmail.com>
2021-09-18 13:36:46 -0300

    teste fix

--- server.js
+++ server.js
@@ -16,3 +16,12 @@ app
 	if (err) { return console.log(err) }
 	console.log('working.')
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
