76a8c92b56f9a8c675037092ba4ba67918bf5a76
lucasSCsantos <luc.cristovam10@gmail.com>
2021-09-18 13:43:16 -0300

    route fix

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ app
 	console.log('working.')
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+if(process.env.NODE_ENV === 'production'){
+	//set static folder
+	app.use(express.static('client/build'));
 }
+app.get('*',(req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
